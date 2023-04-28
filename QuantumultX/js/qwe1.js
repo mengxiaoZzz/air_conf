0c8a8099ab941bad97341285e7e842e04044b5be
guyuqiang <1243952362@qq.com>
2023-04-28 16:58:12 +0800

    update

--- QuantumultX/js/qwe1.js
+++ QuantumultX/js/qwe1.js
@@ -233,7 +233,13 @@ if (_0x2a712a(_0x201ccf['country'])) {
     var _0x3b3570 = _0x4a2b95(_0x201ccf['countryCode']) + '\x20' + _0x43c36a(_0x4d73c5(_0x3bfb89(_0x205810(_0x201ccf['country'])), _0x1ac1e7(_0x205810(_0x201ccf['regionName']), _0x205810(_0x201ccf['city']))));
     var _0x13bc75 = _0x218606(_0x201ccf['timezone']['replace'](/(\_|\-)/g, '\x20'), _0x13c0d8(_0x201ccf['as']), _0x201ccf['query']);
     var _0xb0835f = _0x201ccf['query'];
-    var _0x2cb5ed = '-----------------------------------' + '\x0a\x0a' + _0x4d73c5(_0x3bfb89(_0x205810(_0x201ccf['country'])), _0x1ac1e7(_0x205810(_0x201ccf['regionName']), _0x205810(_0x201ccf['city']))) + '\x0a\x0a' + _0x201ccf['timezone'] + '\x0a\x0a' + _0x201ccf['query'] + '\x0a\x0a' + '经度:' + _0x201ccf['lon'] + '\x20\x20' + '纬度:' + _0x201ccf['lat'] + '\x0a\x0a' + _0x2f6202(_0x201ccf['isp'], _0x201ccf['org']);
+    var _0x2cb5ed = '-----------------------------------'
+        + '\x0a\x0a'
+        + _0x4d73c5(_0x3bfb89(_0x205810(_0x201ccf['country'])), _0x1ac1e7(_0x205810(_0x201ccf['regionName']), _0x205810(_0x201ccf['city'])))
+        + '\x0a\x0a' + _0x201ccf['timezone'] + '\x0a\x0a' + _0x201ccf['query']
+        + '\x0a\x0a' + '经度:' + _0x201ccf['lon'] + '\x20\x20'
+        + '纬度:' + _0x201ccf['lat'] + '\x0a\x0a'
+        + _0x2f6202(_0x201ccf['isp'], _0x201ccf['org']);
 } else {
     var _0x3b3570 = _0x4a2b95(_0x201ccf['countryCode']) + '\x20' + _0x1461fb(_0x201ccf['country'], _0x201ccf['regionName'], _0x201ccf['city']);
     var _0x13bc75 = _0x218606(_0x201ccf['timezone']['replace'](/(\_|\-)/g, '\x20'), _0x13c0d8(_0x201ccf['as']), _0x201ccf['query']);
