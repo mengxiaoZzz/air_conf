1db076846fd43bc072607dad5822ded06b635a5f
guyuqiang <1243952362@qq.com>
2023-11-04 21:55:25 +0800

    update

--- QuantumultX/js/syn.js
+++ QuantumultX/js/syn.js
@@ -1,7 +1,7 @@
 function operator(proxies) {
     let otherCharArray = ['剩余', '套餐', '官', '网', 'GB', 'Traffic', 'Expire']
     let proxies_All = proxies.filter(e => otherCharArray.filter(v => e.name.includes(v)).length === 0)
-    proxies_All.forEach(e => e.name.replace("🇹🇼", "🇨🇳"))
+    proxies_All.forEach(e => e.name = e.name.replace("🇹🇼", "🇨🇳"))
 
     let sortProxies = []
     let proxies_First = []
