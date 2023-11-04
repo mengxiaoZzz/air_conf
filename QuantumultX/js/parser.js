f9d7160820e3f1eb2996a1d0c5240fbdc34cf85d
guyuqiang <1243952362@qq.com>
2023-11-04 14:51:13 +0800

    update

--- QuantumultX/js/parser.js
+++ QuantumultX/js/parser.js
@@ -37,11 +37,13 @@ function operator(proxies) {
     if (subName === 'wait') {
         let proxies_HK_001 = proxies_HK.filter(e => e.name.includes('0.01'))
         let proxies_HK_01 = proxies_HK.filter(e => e.name.includes('0.1'))
+        let proxies_HK_02 = proxies_HK.filter(e => e.name.includes('0.2'))
         let proxies_HK_05 = proxies_HK.filter(e => e.name.includes('0.5'))
-        let proxies_HK_Other = proxies_HK.filter(e => !e.name.includes('0.01') && !e.name.includes('0.1') && !e.name.includes('0.5'))
+        let proxies_HK_Other = proxies_HK.filter(e => !e.name.includes('0.01') && !e.name.includes('0.1') && !e.name.includes('0.2') && !e.name.includes('0.5'))
         proxies_HK = []
         proxies_HK.push(...proxies_HK_001)
         proxies_HK.push(...proxies_HK_01)
+        proxies_HK.push(...proxies_HK_02)
         proxies_HK.push(...proxies_HK_05)
         proxies_HK.push(...proxies_HK_Other)
     }
