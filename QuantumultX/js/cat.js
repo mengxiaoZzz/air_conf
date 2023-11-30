4c92cc80eb11ee5568a087cb345cd5b3d7082abe
guyuqiang <1243952362@qq.com>
2023-11-30 20:27:33 +0800

    update

--- QuantumultX/js/cat.js
+++ QuantumultX/js/cat.js
@@ -1,4 +1,6 @@
 let body = JSON.parse($response.body);
+console.log($response.body)
+console.log(body)
 body.data.advertisement = []
 body.data.index.is_pay = 1
 body = JSON.stringify(body);
--- QuantumultX/js/cat1.js
+++ QuantumultX/js/cat1.js
@@ -1,4 +1,6 @@
 let body = JSON.parse($response.body);
+console.log($response.body)
+console.log(body)
 body.data.ad_video = ""
 body = JSON.stringify(body);
 $done({body});
