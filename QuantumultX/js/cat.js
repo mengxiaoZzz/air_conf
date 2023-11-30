21a666993ed84f0bbeb08de3d931afb8a555ae38
guyuqiang <1243952362@qq.com>
2023-11-30 20:44:19 +0800

    update

--- QuantumultX/js/cat.js
+++ QuantumultX/js/cat.js
@@ -3,5 +3,6 @@ let body = $response.body;
 console.log(`body`)
 console.log(body)
 //body.data.advertisement = []
-body.data.index.is_pay = 1
+body.data.index.app_name = "一只猫去水印Pro"
+//body.data.index.is_pay = 1
 $done({body});
