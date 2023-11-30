4f94b641375ea38f2d4027b822c412d66c3df296
guyuqiang <1243952362@qq.com>
2023-11-30 21:10:32 +0800

    update

--- QuantumultX/js/cat.js
+++ QuantumultX/js/cat.js
@@ -3,5 +3,7 @@ let body = JSON.parse($response.body);
 body.data.advertisement = []
 body.data.index.app_name = "一只猫去水印Pro"
 body.data.index.is_pay = 1
+body.data.index.invite_num = 999
+body.data.index.level = 999
 body = JSON.stringify(body);
 $done({body});
