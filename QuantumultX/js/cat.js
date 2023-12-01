729163539806a51bfa599343fb6a21201e6f8dfa
guyuqiang <1243952362@qq.com>
2023-12-01 09:37:48 +0800

    update

--- QuantumultX/js/cat.js
+++ QuantumultX/js/cat.js
@@ -5,16 +5,13 @@ if (url.includes("/api/wechat_v2/index")) {
     body.data.advertisement = []
     body.data.index.app_name = "一只猫去水印Pro"
     body.data.index.is_pay = 1
-    body.data.index.invite_num = 999
-    body.data.index.level = 999
-    body.data.index.vip_ad = "unchecked"
 }
 if (url.includes("/api/wechat_v2/stars")) {
     body.data.ad = ""
 }
 if (url.includes("/api/wechat_v2/user")) {
     body.name = "一只猫去水印Pro"
-    body.invite_num = 999
+    body.invite_num = 9999
     body.svip = 1
     body.residue_time = "2099-12-31 23:59"
     body.data.is_svip = 1
