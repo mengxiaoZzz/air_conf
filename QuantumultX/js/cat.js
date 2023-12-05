55d5edf10f269c2833760794ff027dbfa3a8e459
guyuqiang <1243952362@qq.com>
2023-12-05 17:12:20 +0800

    update

--- QuantumultX/js/cat.js
+++ QuantumultX/js/cat.js
@@ -1,19 +1,20 @@
 // console.log(`一只猫去水印`);
 const url = $request.url;
+const name = "一只猫去水印Pro";
 let body = JSON.parse($response.body);
 if (url.includes("/api/wechat_v2/index")) {
     body.data.advertisement = []
-    body.data.index.app_name = "一只猫去水印Pro"
+    body.data.index.app_name = name
     body.data.index.is_pay = 1
 }
 if (url.includes("/api/wechat_v2/stars")) {
     body.data.ad = ""
 }
 if (url.includes("/api/wechat_v2/user")) {
-    body.name = "一只猫去水印Pro"
+    body.name = name
     body.invite_num = 9999
     body.svip = 1
-    body.residue_time = "2099-12-31 23:59"
+    body.residue_time = "9999-12-31 23:59"
     body.data.is_svip = 1
 }
 body = JSON.stringify(body);
