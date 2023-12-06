3f8c5c5e9d0c5c46c3bf8ed5ac9c26b37677c6cd
guyuqiang <1243952362@qq.com>
2023-12-06 09:13:27 +0800

    update

--- QuantumultX/js/linkcube.js
+++ QuantumultX/js/linkcube.js
@@ -3,6 +3,36 @@ function operator(proxies) {
     let proxies_All = proxies.filter(e => otherCharArray.filter(v => e.name.includes(v)).length === 0)
     proxies_All.forEach(e => {
         e.name = e.name.replace("专线 深圳-", "")
+        e.name = e.name.replace("香港", "HK")
+        e.name = e.name.replace("台湾", "TW")
+        e.name = e.name.replace("狮城", "SG")
+        e.name = e.name.replace("日本", "JP")
+        e.name = e.name.replace("美国", "US")
     })
-    return proxies_All;
+
+    let sortProxies = []
+
+    let proxies_HK = proxies_All.filter(e => e.name.includes('🇭🇰'))
+    proxies_All = proxies_All.filter(e => !e.name.includes('🇭🇰'))
+
+    let proxies_TW = proxies_All.filter(e => e.name.includes('🇨🇳'))
+    proxies_All = proxies_All.filter(e => !e.name.includes('🇨🇳'))
+
+    let proxies_SG = proxies_All.filter(e => e.name.includes('🇸🇬'))
+    proxies_All = proxies_All.filter(e => !e.name.includes('🇸🇬'))
+
+    let proxies_JP = proxies_All.filter(e => e.name.includes('🇯🇵'))
+    proxies_All = proxies_All.filter(e => !e.name.includes('🇯🇵'))
+
+    let proxies_US = proxies_All.filter(e => e.name.includes('🇺🇸'))
+    proxies_All = proxies_All.filter(e => !e.name.includes('🇺🇸'))
+
+    sortProxies.push(...proxies_HK)
+    sortProxies.push(...proxies_TW)
+    sortProxies.push(...proxies_SG)
+    sortProxies.push(...proxies_JP)
+    sortProxies.push(...proxies_US)
+    sortProxies.push(...proxies_All)
+
+    return sortProxies;
 }
