16286aa651ef7b727d4adb4633792d53accecfd2
guyuqiang <1243952362@qq.com>
2023-12-06 17:44:16 +0800

    update

--- QuantumultX/js/wait.js
+++ QuantumultX/js/wait.js
@@ -1,21 +1,24 @@
 function operator(proxies) {
     let otherCharArray = ['剩余', '套餐', '官', '网', 'GB', 'Traffic', 'Expire']
     let proxies_All = proxies.filter(e => otherCharArray.filter(v => e.name.includes(v)).length === 0)
-    proxies_All.forEach(e => e.name = e.name.replace("🇹🇼", "🇨🇳"))
+    proxies_All.forEach(function (e, index) {
+        e.name = e.name.replace("🇹🇼", "🇨🇳")
+        e.name = e.name.replace("[", "")
+        e.name = e.name.substring(0, 16)
+        if (e.name.includes('台湾')) {
+            e.name = e.name + (index % 2 + 1)
+        }
+    })
 
     let sortProxies = []
-    let proxies_First = []
 
     let proxies_HK = proxies_All.filter(e => e.name.includes('🇭🇰'))
 
     let proxies_HK_001 = proxies_HK.filter(e => e.name.includes('0.01'))
-    let proxies_HK_01 = proxies_HK.filter(e => e.name.includes('0.1'))
     let proxies_HK_02 = proxies_HK.filter(e => e.name.includes('0.2'))
-    let proxies_HK_Other = proxies_HK.filter(e => !e.name.includes('0.01') && !e.name.includes('0.1')
-        && !e.name.includes('0.2'))
+    let proxies_HK_Other = proxies_HK.filter(e => !e.name.includes('0.01') && !e.name.includes('0.2'))
     proxies_HK = []
     proxies_HK.push(...proxies_HK_001)
-    proxies_HK.push(...proxies_HK_01)
     proxies_HK.push(...proxies_HK_02)
     proxies_HK.push(...proxies_HK_Other)
 
@@ -33,7 +36,6 @@ function operator(proxies) {
     let proxies_US = proxies_All.filter(e => e.name.includes('🇺🇸'))
     proxies_All = proxies_All.filter(e => !e.name.includes('🇺🇸'))
 
-    sortProxies.push(...proxies_First)
     sortProxies.push(...proxies_HK)
     sortProxies.push(...proxies_TW)
     sortProxies.push(...proxies_SG)
