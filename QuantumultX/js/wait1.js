c2167d75de1a41ee88f4bb10ae5b49bd66b4423c
guyuqiang <1243952362@qq.com>
2023-12-11 09:29:08 +0800

    update

--- QuantumultX/js/wait1.js
+++ QuantumultX/js/wait1.js
@@ -8,6 +8,13 @@ function operator(proxies) {
         e.name = e.name.replace(" | ", "|")
         e.name = e.name.replace("0.20x", "0.2x").replace("1.00x", "1.0x").replace("2.00x", "2.0x").replace("30.0x", "30x")
         e.name = e.name.replace("香港", "HK").replace("台湾", "TW").replace("狮城", "SG").replace("日本", "JP").replace("美国", "US")
+
+        let name = e.name.substring(5);
+        let flag = e.name.substring(0, 4);
+        let country = name.split(' ')[0]
+        let rate = name.split(' ')[1]
+        let line = name.split(' ')[2]
+        e.name = flag + ' ' + country + ' ' + line + ' ' + rate;
     })
 
     let sortProxies = []
