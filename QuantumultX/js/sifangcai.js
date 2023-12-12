9d1a8a5b8fd5c66c1927b215c5e2b7d02f710c39
guyuqiang <1243952362@qq.com>
2023-12-12 16:07:52 +0800

    update

--- QuantumultX/js/sifangcai.js
+++ QuantumultX/js/sifangcai.js
@@ -1,5 +1,5 @@
 function operator(proxies) {
-    let otherCharArray = ['剩余', '套餐', '官', '网', 'GB', 'Traffic', 'Expire', '📝', 'Yourip', 'ISP']
+    let otherCharArray = ['剩余', '套餐', '官', '网', 'GB', 'Traffic', 'Expire', '📝', 'Yourip', 'ISP', '攻击']
     let proxies_All = proxies.filter(e => otherCharArray.filter(v => e.name.includes(v)).length === 0)
     proxies_All.forEach(e => e.name = e.name.replace("🇹🇼", "🇨🇳"))
     return proxies_All;
