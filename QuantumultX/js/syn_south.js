4ca69f4df2563a1728f2af4510c4156590dcf64b
guyuqiang <1243952362@qq.com>
2023-12-14 11:21:29 +0800

    update

--- QuantumultX/js/syn_south.js
+++ QuantumultX/js/syn_south.js
@@ -1,7 +1,12 @@
 function operator(proxies) {
     let otherCharArray = ['剩余', '套餐', '官', '网', 'GB', 'Traffic', 'Expire']
     let proxies_All = proxies.filter(e => otherCharArray.filter(v => e.name.includes(v)).length === 0)
-    proxies_All.forEach(e => e.name = e.name.replace("🇹🇼", "🇨🇳"))
+    proxies_All.forEach(e => {
+        e.name = e.name.replace("🇹🇼", "🇨🇳")
+        if (!e.name.includes('SP')) {
+            e.server = 'traffic-in-03.811920.xyz'
+        }
+    })
 
     let sortProxies = []
 
