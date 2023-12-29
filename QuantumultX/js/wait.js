aa3155a1e48629a94cecdd2375f5f339ea821499
guyuqiang <1243952362@qq.com>
2023-12-29 17:11:01 +0800

    update

--- QuantumultX/js/wait.js
+++ QuantumultX/js/wait.js
@@ -7,7 +7,7 @@ function operator(proxies) {
         e.name = e.name.replace("]", "")
         e.name = e.name.replace("  | ", "·")
         e.name = e.name.replace(" | ", "·")
-        e.name = e.name.replace("0.20x", "0.2x").replace("1.00x", "1.0x").replace("2.00x", "2.0x").replace("30.0x", "30x")
+        e.name = e.name.replace("0.20x", "0.2x").replace("1.00x", "1x").replace("2.00x", "2x").replace("30.0x", "30x")
         e.name = e.name.replace("香港", "HK").replace("台湾", "TW").replace("狮城", "SG").replace("日本", "JP").replace("美国", "US")
 
         let name = e.name.substring(5);
@@ -21,9 +21,9 @@ function operator(proxies) {
     let sortProxies = []
 
     let proxies_HK = proxies_All.filter(e => e.name.includes('🇭🇰'))
-    let proxies_HK_20 = proxies_HK.filter(e => e.name.includes('2.0'))
-    let proxies_HK_10 = proxies_HK.filter(e => e.name.includes('1.0'))
-    let proxies_HK_02 = proxies_HK.filter(e => e.name.includes('0.2'))
+    let proxies_HK_20 = proxies_HK.filter(e => e.name.includes('2x'))
+    let proxies_HK_10 = proxies_HK.filter(e => e.name.includes('1x'))
+    let proxies_HK_02 = proxies_HK.filter(e => e.name.includes('0.2x'))
     let proxies_HK_EIP = proxies_HK.filter(e => e.name.includes('EIP'))
     proxies_HK = []
     proxies_HK.push(...proxies_HK_20)
@@ -41,9 +41,9 @@ function operator(proxies) {
     proxies_All = proxies_All.filter(e => !e.name.includes('🇨🇳'))
 
     let proxies_SG = proxies_All.filter(e => e.name.includes('🇸🇬'))
-    let proxies_SG_20 = proxies_SG.filter(e => e.name.includes('2.0'))
-    let proxies_SG_10 = proxies_SG.filter(e => e.name.includes('1.0'))
-    let proxies_SG_02 = proxies_SG.filter(e => e.name.includes('0.2'))
+    let proxies_SG_20 = proxies_SG.filter(e => e.name.includes('2x'))
+    let proxies_SG_10 = proxies_SG.filter(e => e.name.includes('1x'))
+    let proxies_SG_02 = proxies_SG.filter(e => e.name.includes('0.2x'))
     proxies_SG = []
     proxies_SG.push(...proxies_SG_20)
     proxies_SG.push(...proxies_SG_10)
@@ -51,9 +51,9 @@ function operator(proxies) {
     proxies_All = proxies_All.filter(e => !e.name.includes('🇸🇬'))
 
     let proxies_JP = proxies_All.filter(e => e.name.includes('🇯🇵'))
-    let proxies_JP_20 = proxies_JP.filter(e => e.name.includes('2.0'))
-    let proxies_JP_10 = proxies_JP.filter(e => e.name.includes('1.0'))
-    let proxies_JP_02 = proxies_JP.filter(e => e.name.includes('0.2'))
+    let proxies_JP_20 = proxies_JP.filter(e => e.name.includes('2x'))
+    let proxies_JP_10 = proxies_JP.filter(e => e.name.includes('1x'))
+    let proxies_JP_02 = proxies_JP.filter(e => e.name.includes('0.2x'))
     proxies_JP = []
     proxies_JP.push(...proxies_JP_20)
     proxies_JP.push(...proxies_JP_10)
@@ -61,9 +61,9 @@ function operator(proxies) {
     proxies_All = proxies_All.filter(e => !e.name.includes('🇯🇵'))
 
     let proxies_US = proxies_All.filter(e => e.name.includes('🇺🇸'))
-    let proxies_US_20 = proxies_US.filter(e => e.name.includes('2.0'))
-    let proxies_US_10 = proxies_US.filter(e => e.name.includes('1.0'))
-    let proxies_US_02 = proxies_US.filter(e => e.name.includes('0.2'))
+    let proxies_US_20 = proxies_US.filter(e => e.name.includes('2x'))
+    let proxies_US_10 = proxies_US.filter(e => e.name.includes('1x'))
+    let proxies_US_02 = proxies_US.filter(e => e.name.includes('0.2x'))
     proxies_US = []
     proxies_US.push(...proxies_US_20)
     proxies_US.push(...proxies_US_10)
