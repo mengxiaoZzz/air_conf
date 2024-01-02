ea6287b17dfafebe90f8a72ec66265288da7c184
guyuqiang <1243952362@qq.com>
2024-01-02 10:56:08 +0800

    update

--- Surge/js/bilibili-json.js
+++ Surge/js/bilibili-json.js
@@ -23,7 +23,7 @@ if (!body.data) {
             // 有时候返回的数据没有show字段
             console.log('数据无show字段');
         } else {
-            body.data.show = []
+            delete body.data.show;
             console.log('成功');
         }
     } else if (url.includes("resource/show/tab/v2")) {
