62cbdfae46dd7fe9aff8f74869d5ba3416f43c78
guyuqiang <1243952362@qq.com>
2024-01-18 09:47:19 +0800

    update

---
+++ QuantumultX/js/Sakana.js
@@ -0,0 +1,31 @@
+function operator(proxies) {
+    let otherCharArray = ['剩余', '套餐', '距离']
+    let proxies_All = proxies.filter(e => otherCharArray.filter(v => e.name.includes(v)).length === 0)
+    proxies_All.forEach(e => e.name = e.name.replace("🇹🇼", "🇨🇳"))
+
+    let sortProxies = []
+
+    let proxies_HK = proxies_All.filter(e => e.name.includes('🇭🇰'))
+    proxies_All = proxies_All.filter(e => !e.name.includes('🇭🇰'))
+
+    let proxies_TW = proxies_All.filter(e => e.name.includes('🇨🇳'))
+    proxies_All = proxies_All.filter(e => !e.name.includes('🇨🇳'))
+
+    let proxies_SG = proxies_All.filter(e => e.name.includes('🇸🇬'))
+    proxies_All = proxies_All.filter(e => !e.name.includes('🇸🇬'))
+
+    let proxies_JP = proxies_All.filter(e => e.name.includes('🇯🇵'))
+    proxies_All = proxies_All.filter(e => !e.name.includes('🇯🇵'))
+
+    let proxies_US = proxies_All.filter(e => e.name.includes('🇺🇸'))
+    proxies_All = proxies_All.filter(e => !e.name.includes('🇺🇸'))
+
+    sortProxies.push(...proxies_HK)
+    sortProxies.push(...proxies_TW)
+    sortProxies.push(...proxies_SG)
+    sortProxies.push(...proxies_JP)
+    sortProxies.push(...proxies_US)
+    sortProxies.push(...proxies_All)
+
+    return sortProxies;
+}
