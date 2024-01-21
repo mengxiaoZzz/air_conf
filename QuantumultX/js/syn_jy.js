505a1ccadb3b0ae93c3d70846b46b68ff542f62e
guyuqiang <1243952362@qq.com>
2024-01-21 20:37:19 +0800

    update

--- QuantumultX/js/syn_jy.js
+++
@@ -1,6 +0,0 @@
-function operator(proxies) {
-    proxies_All.forEach(e => {
-        e.server = 'traffic-in-04.811920.xyz'
-    })
-    return proxies_All;
-}
--- QuantumultX/js/syn_sy.js
+++ QuantumultX/js/syn_sy.js
@@ -2,7 +2,7 @@ function operator(proxies) {
     let proxies_All = proxies
     proxies_All.forEach(e => {
         e.name = e.name.replace("🇹🇼", "🇨🇳")
-        e.server = 'traffic-in-03.811920.xyz'
+        e.server = 'traffic-in-04.811920.xyz'
     })
 
     let sortProxies = []
@@ -36,10 +36,10 @@ function operator(proxies) {
     sortProxies.unshift({name: '🇱🇺 LU 硝酸二甲酯 0.01x', type, server: "traffic-in-lite.811920.xyz", port: 50009, cipher, password, udp})
     sortProxies.unshift({name: '🇸🇬 SG 苯巴比妥钠 0.01x', type, server: "traffic-in-lite.811920.xyz", port: 50012, cipher, password, udp})
     sortProxies.unshift({name: '🇭🇰 HK 丁香酰氧胺 0.01x', type, server: "traffic-in-lite.811920.xyz", port: 50013, cipher, password, udp})
-    sortProxies.unshift({name: '🇸🇬 TG 碳酸二甲酯', type, server: "traffic-in-03.811920.xyz", port: 54001, cipher, password, udp})
-    sortProxies.unshift({name: '🇲🇴 TK 澳士蛋白酶 3x', type, server: "traffic-in-03.811920.xyz", port: 54001, cipher, password, udp})
-    sortProxies.unshift({name: '🇯🇵 TK 日库木糖醇 3x', type, server: "traffic-in-03.811920.xyz", port: 54001, cipher, password, udp})
-    //sortProxies.unshift({name: '🇺🇸 TK 美罗培南素 3x', type, server: "traffic-in-03.811920.xyz", port: 54001, cipher, password, udp})
+    sortProxies.unshift({name: '🇸🇬 TG 碳酸二甲酯', type, server: "traffic-in-04.811920.xyz", port: 54001, cipher, password, udp})
+    sortProxies.unshift({name: '🇲🇴 TK 澳士蛋白酶 3x', type, server: "traffic-in-04.811920.xyz", port: 54001, cipher, password, udp})
+    sortProxies.unshift({name: '🇯🇵 TK 日库木糖醇 3x', type, server: "traffic-in-04.811920.xyz", port: 54001, cipher, password, udp})
+    //sortProxies.unshift({name: '🇺🇸 TK 美罗培南素 3x', type, server: "traffic-in-04.811920.xyz", port: 54001, cipher, password, udp})
 
     return sortProxies;
 }
