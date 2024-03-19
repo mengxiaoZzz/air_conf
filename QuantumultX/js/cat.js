55436681cfb42638ce802453015882c288bdeaa1
guyuqiang <1243952362@qq.com>
2024-03-19 10:25:25 +0800

    Changes

--- QuantumultX/js/cat.js
+++ QuantumultX/js/cat.js
@@ -2,15 +2,15 @@
 const url = $request.url;
 const name = "一只猫去水印Pro";
 let body = JSON.parse($response.body);
-if (url.includes("/api/wechat_v2/index")) {
+if (url.includes("/api/Mushroom/index")) {
     body.data.advertisement = []
     body.data.index.app_name = name
     body.data.index.is_pay = 1
 }
-if (url.includes("/api/wechat_v2/stars")) {
+if (url.includes("/api/Mushroom/stars")) {
     body.data.ad = ""
 }
-if (url.includes("/api/wechat_v2/user")) {
+if (url.includes("/api/Mushroom/user")) {
     body.name = name
     body.invite_num = 9999
     body.svip = 1
