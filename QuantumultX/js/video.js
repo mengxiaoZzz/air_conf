e683c397f428b95fb8c52bc7fb0f8cfa6933b00f
guyuqiang <1243952362@qq.com>
2024-06-06 17:57:37 +0800

    Changes

--- QuantumultX/js/video.js
+++ QuantumultX/js/video.js
@@ -2,6 +2,7 @@ const url = $request.url;
 let body = JSON.parse($response.body);
 if (url.includes("/api/v1/?act=get_config")) {
     body.data.user.vip = true
+    body.data.vipvalue = true
     body.data.user.vip_deltime = "2099-12-31 24:59:59"
     body.data.ad = { "ad1": null }
 }
