237d04c3b46829103fb7e208f22a806a38a53b20
guyuqiang <1243952362@qq.com>
2024-06-07 11:00:05 +0800

    Changes

--- QuantumultX/js/video.js
+++ QuantumultX/js/video.js
@@ -3,13 +3,13 @@ let body = JSON.parse($response.body);
 if (url.includes("/api/v1/?act=get_config")) {
     body.data.vipvalue = true
     body.data.user.vip = true
-    body.data.user.vip_deltime = "2025-06-07 24:59:59"
+    body.data.user.vip_deltime = "2030-12-31 24:59:59"
     body.data.ad = {"ad1": null}
     body.data.stick = true
 }
 if (url.includes("/api/v1/?act=user")) {
     body.data.user.vip = true
-    body.data.user.vip_deltime = "2025-06-07 24:59:59"
+    body.data.user.vip_deltime = "2030-12-31 24:59:59"
 }
 if (url.includes("/api/v1/?act=detail")) {
     let videoList = body.data.videos.reverse()
