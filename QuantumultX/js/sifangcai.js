53767a14ed0325d59320b8ad95879a686939bb11
guyuqiang <1243952362@qq.com>
2024-07-04 11:23:04 +0800

    Changes

--- QuantumultX/js/sifangcai.js
+++
@@ -1,5 +0,0 @@
-function operator(proxies) {
-    let proxies_All = proxies.filter(e => !e.name.includes('📝'))
-    proxies_All.forEach(e => e.name = e.name.replace("🇹🇼", "🇨🇳"))
-    return proxies_All;
-}
