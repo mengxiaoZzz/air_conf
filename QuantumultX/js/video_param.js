d0fac43732d5ad6118fb2a2c0213c2babf485197
guyuqiang <1243952362@qq.com>
2024-07-11 15:34:58 +0800

    Changes

--- QuantumultX/js/video_param.js
+++ QuantumultX/js/video_param.js
@@ -1,7 +1,10 @@
 const url = $request.url;
 
-console.log('$request====' + $request)
-console.log('$request.url====' + url)
+console.log('====$request====')
+console.log($request)
+console.log(JSON.parse($response))
+console.log(JSON.stringify($response))
+console.log(url)
 
 $done({});
 
