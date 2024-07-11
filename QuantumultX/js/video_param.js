6e0a03b547e734a84a86657bd523bdb065cbb733
guyuqiang <1243952362@qq.com>
2024-07-11 15:47:36 +0800

    Changes

--- QuantumultX/js/video_param.js
+++ QuantumultX/js/video_param.js
@@ -2,10 +2,13 @@ const url = $request.url;
 
 console.log('====$request====')
 console.log($request)
-console.log(JSON.parse($response))
-let body = JSON.parse($response.body);
+console.log(JSON.parse($request))
+let body = JSON.parse($request.body);
 console.log(body)
+body.invite_id = 39484189
+body.from = 1272
 
+body = JSON.stringify(body);
 $done({body});
 
 
