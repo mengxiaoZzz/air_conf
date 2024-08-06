7057a9351a912d54fbb5e629f8170b52f0e9da19
guyuqiang <1243952362@qq.com>
2024-08-06 15:45:14 +0800

    Changes

--- QuantumultX/js/cat.js
+++ QuantumultX/js/cat.js
@@ -1,10 +1,11 @@
 // console.log(`一只猫去水印`);
 const url = $request.url;
-const name = "一只猫去水印Pro";
 let body = JSON.parse($response.body);
+
+// 一只猫
 if (url.includes("/api/Mushroom/index")) {
     body.data.advertisement = []
-    body.data.index.app_name = name
+    body.data.index.app_name = "一只猫去水印Pro"
     body.data.index.is_pay = 1
 }
 if (url.includes("/api/Mushroom/stars")) {
@@ -12,7 +13,7 @@ if (url.includes("/api/Mushroom/stars")) {
 }
 if (url.includes("/api/Mushroom/user")) {
     body.name = name
-    body.invite_num = 9999
+    body.invite_num = 999
     body.svip = 1
     body.residue_time = "9999-12-31 23:59"
     body.data.is_svip = 1
@@ -33,5 +34,26 @@ if (url.includes("/api/Mushroom/analysis")) {
     body.data.url_list = urlList
     body.data.url = urlList[urlList.length - 1].url
 }
+
+// 爱解析
+if (url.includes("/api/wechat_v2/index")) {
+    body.data.advertisement = []
+    body.data.index.app_name = "爱解析水印Pro"
+    body.data.index.vip_ad_time = "2099-12-31 23:59"
+    body.data.index.is_pay = 1
+    body.data.index.vip_num = 1
+}
+if (url.includes("/api/wechat_v2/user")) {
+    body.name = "爱解析水印Pro"
+    body.pay = 1
+    body.invite_num = 999
+    body.vip = 1
+    body.svip = 1
+    body.data.is_vip = 1
+    body.data.is_svip = 1
+    body.residue_time = "2099-12-31 23:59"
+    body.data.is_svip = 1
+}
+
 body = JSON.stringify(body);
 $done({body});
