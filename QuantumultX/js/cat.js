e90c519635f74268ce662220e405b86337874f4c
guyuqiang <1243952362@qq.com>
2024-08-22 16:21:34 +0800

    Changes

--- QuantumultX/js/cat.js
+++ QuantumultX/js/cat.js
@@ -24,43 +24,14 @@ if (url.includes("/api/Mushroom/urlresult")) {
 }
 if (url.includes("/api/Mushroom/analysis")) {
     let urlList = body.data.url_list
-    // // 原视频
-    // let rawVideo = urlList.filter(e => e.def === '原视频')
-    // if (rawVideo.length > 0) {
-    //     urlList = urlList.filter(e => e.def !== '原视频')
-    //     urlList.push(...rawVideo)
-    // }
+    if (urlList != null) {
+        urlList.sort((a, b) => {
+            return a.size - b.size
+        });
 
-    urlList.sort((a, b) => {
-        return a.size - b.size
-    });
-
-    body.data.url_list = urlList
-    body.data.url = urlList[urlList.length - 1].url
-}
-
-// 爱解析
-if (url.includes("/api/wechat_v2/index")) {
-    body.data.advertisement = []
-    body.data.index.app_name = "爱解析水印Pro"
-    body.data.index.vip_ad_time = "2099-12-31 23:59"
-    body.data.index.is_pay = 1
-    body.data.index.vip_num = 1
-}
-if (url.includes("/api/wechat_v2/urlresult")) {
-    body.data.vip = 1
-    body.data.ad_video = ""
-}
-if (url.includes("/api/wechat_v2/user")) {
-    body.name = "爱解析水印Pro"
-    body.pay = 1
-    body.invite_num = 999
-    body.vip = 1
-    body.svip = 1
-    body.data.is_vip = 1
-    body.data.is_svip = 1
-    body.residue_time = "2099-12-31 23:59"
-    body.data.is_svip = 1
+        body.data.url_list = urlList
+        body.data.url = urlList[urlList.length - 1].url
+    }
 }
 
 body = JSON.stringify(body);
