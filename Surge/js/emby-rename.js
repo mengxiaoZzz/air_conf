53689dba0a89e8781acbf83430c643d65dbdf639
baixiaofei <guyuqiang@qq.com>
2025-05-22 10:01:29 +0800

    update

--- Surge/js/emby-rename.js
+++ Surge/js/emby-rename.js
@@ -97,34 +97,5 @@ if (url.includes("chilledmelon")) {
     });
 }
 
-if (url.includes("worldline")) {
-    body.Items.forEach(item => {
-        item.Name = item.Name.slice(3)
-        item.ForcedSortName = item.Name
-        item.SortName = item.Name
-    })
-    // 按此顺序排序
-    const customOrder = [
-        "国漫",
-        "动漫",
-        "国产剧",
-        "日韩剧",
-        "欧美剧",
-        "华语电影",
-        "外语电影",
-        "动画电影",
-        "记录电影",
-        "综艺",
-        "纪录片",
-        "日本中字",
-        "水果派",
-        "次元里番",
-        "经典三级"
-    ];
-    body.Items = body.Items.sort((a, b) => {
-        return customOrder.indexOf(a.Name) - customOrder.indexOf(b.Name);
-    });
-}
-
 body = JSON.stringify(body);
 $done({body});
