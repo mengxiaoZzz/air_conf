3570193f00af6175e87d2879a3ae2f703e037223
baixiaofei <guyuqiang@qq.com>
2025-06-12 10:11:42 +0800

    update

--- Surge/js/EmbyBitrateSort.js
+++ Surge/js/EmbyBitrateSort.js
@@ -7,7 +7,7 @@ let body = JSON.parse($response.body);
 let items = body.Items;
 items.forEach(item => {
     item.MediaSources = item.MediaSources.sort((a, b) => {
-        return a.Bitrate - b.Bitrate;
+        return b.Bitrate - a.Bitrate;
     })
 })
 body.Items = items;
