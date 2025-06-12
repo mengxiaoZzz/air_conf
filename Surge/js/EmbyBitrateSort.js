a556ccd88ce9aedb2a17d102296357e7e1c9b52f
baixiaofei <guyuqiang@qq.com>
2025-06-12 10:12:15 +0800

    update

--- Surge/js/EmbyBitrateSort.js
+++ Surge/js/EmbyBitrateSort.js
@@ -1,9 +1,6 @@
 const url = $request.url;
 let body = JSON.parse($response.body);
 
-// console.log('emby-rename');
-// console.log('url==>' + url);
-
 let items = body.Items;
 items.forEach(item => {
     item.MediaSources = item.MediaSources.sort((a, b) => {
