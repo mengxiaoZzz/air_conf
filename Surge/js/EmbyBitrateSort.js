f21dc07a19535a3c751a2cbccb4f5abba5b93bdb
baixiaofei <guyuqiang@qq.com>
2025-06-16 10:45:01 +0800

    update

--- Surge/js/EmbyBitrateSort.js
+++ Surge/js/EmbyBitrateSort.js
@@ -3,6 +3,13 @@ let body = JSON.parse($response.body);
 
 let items = body.Items;
 items.forEach(item => {
+    // 筛选-剔除过低码率
+    let MediaSources = item.MediaSources.filter(e => e.Bitrate >= 1024);
+    if (MediaSources.length > 0) {
+        item.MediaSources = MediaSources;
+    }
+
+    // 排序
     item.MediaSources = item.MediaSources.sort((a, b) => {
         return b.Bitrate - a.Bitrate;
     })
