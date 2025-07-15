3ef34da22223280a6a08860ba9789b5f2c802935
baixiaofei <guyuqiang@qq.com>
2025-07-15 10:02:10 +0800

    update

--- QuantumultX/js/cat.js
+++ QuantumultX/js/cat.js
@@ -1,45 +1,48 @@
 const url = $request.url;
 let body = JSON.parse($response.body);
 
-console.log('body');
-console.log(body);
 
+try {
 // 一只猫
-if (url.includes("/api/Mushroom/index") || url.includes("/api/wechat_v2/index")) {
-    body.data.advertisement = []
-    body.data.index.app_name = "一只猫去水印Pro"
-    body.data.index.is_pay = 1
-}
-if (url.includes("/api/Mushroom/stars") || url.includes("/api/wechat_v2/stars")) {
-    body.data.ad = ""
-}
-if (url.includes("/api/Mushroom/user") || url.includes("/api/wechat_v2/user")) {
-    body.name = name
-    body.invite_num = 999
-    body.svip = 1
-    body.residue_time = "9999-12-31 23:59"
-    body.data.is_svip = 1
-}
-if (url.includes("/api/Mushroom/urlresult") || url.includes("/api/wechat_v2/urlresult")) {
-    body.data.vip = 1
-    body.data.ad_video = ""
-}
-if (url.includes("/api/Mushroom/analysis") || url.includes("/api/wechat_v2/analysis")) {
-    console.log('body.data');
-    console.log(body.data);
-    console.log(body.data.url_list);
-
-
-    if (body.data != null && body.data.url_list != null) {
-        let urlList = body.data.url_list
-        urlList.sort((a, b) => {
-            return a.size - b.size
-        });
-
-        body.data.url_list = urlList
-        body.data.url = urlList[urlList.length - 1].url
+    if (url.includes("/api/Mushroom/index") || url.includes("/api/wechat_v2/index")) {
+        body.data.advertisement = []
+        body.data.index.app_name = "一只猫去水印Pro"
+        body.data.index.is_pay = 1
     }
-}
+    if (url.includes("/api/Mushroom/stars") || url.includes("/api/wechat_v2/stars")) {
+        body.data.ad = ""
+    }
+    if (url.includes("/api/Mushroom/user") || url.includes("/api/wechat_v2/user")) {
+        body.name = name
+        body.invite_num = 999
+        body.svip = 1
+        body.residue_time = "9999-12-31 23:59"
+        body.data.is_svip = 1
+    }
+    if (url.includes("/api/Mushroom/urlresult") || url.includes("/api/wechat_v2/urlresult")) {
+        body.data.vip = 1
+        body.data.ad_video = ""
+    }
+    if (url.includes("/api/Mushroom/analysis") || url.includes("/api/wechat_v2/analysis")) {
+        console.log('body.data');
+        console.log(body.data);
+        console.log(body.data.url_list);
 
-body = JSON.stringify(body);
-$done({body});
+
+        if (body.data != null && body.data.url_list != null) {
+            let urlList = body.data.url_list
+            urlList.sort((a, b) => {
+                return a.size - b.size
+            });
+
+            body.data.url_list = urlList
+            body.data.url = urlList[urlList.length - 1].url
+        }
+    }
+
+    body = JSON.stringify(body);
+    $done({body});
+} catch (e) {
+    body = JSON.stringify(body);
+    $done({body});
+}
--- QuantumultX/js/video.js
+++ QuantumultX/js/video.js
@@ -1,9 +1,8 @@
 const url = $request.url;
 let body = JSON.parse($response.body);
 
-console.log('body');
-console.log(body);
 
+try {
 // if (url.includes("/api/v1/?act=get_config")) {
 //     body.data.vipvalue = true
 //     body.data.user.vip = true
@@ -15,41 +14,44 @@ console.log(body);
 //     body.data.user.vip = true
 //     body.data.user.vip_deltime = "2030-12-31 23:59:59"
 // }
-if (url.includes("/api/v1/?act=detail") && body.data && body.data.video_list) {
-    let videoList = body.data.video_list
+    if (url.includes("/api/v1/?act=detail") && body.data && body.data.video_list) {
+        let videoList = body.data.video_list
 
-    videoList.forEach(item => {
-        item.level = item.level.replace("Kb", "KB").replace("Mb", "MB").replace(" (", "「").replace(")", "」")
-    })
+        videoList.forEach(item => {
+            item.level = item.level.replace("Kb", "KB").replace("Mb", "MB").replace(" (", "「").replace(")", "」")
+        })
 
-    // // 定义优先级顺序
-    // const priorityOrder = {
-    //     "原画": 100,
-    //     "1080p": 90,
-    //     "720p": 80
-    // };
-    //
-    // // 排序函数
-    // videoList.sort((a, b) => {
-    //     const getKey = (str) => {
-    //         if (str.includes("原画")) return "原画";
-    //         if (str.includes("1080p")) return "1080p";
-    //         if (str.includes("720p")) return "720p";
-    //         if (str.includes("480p")) return "480p";
-    //         return str; // 其他情况
-    //     };
-    //
-    //     const keyA = getKey(a.level);
-    //     const keyB = getKey(b.level);
-    //
-    //     return (priorityOrder[keyA] || 10) - (priorityOrder[keyB] || 10);
-    // });
+        // // 定义优先级顺序
+        // const priorityOrder = {
+        //     "原画": 100,
+        //     "1080p": 90,
+        //     "720p": 80
+        // };
+        //
+        // // 排序函数
+        // videoList.sort((a, b) => {
+        //     const getKey = (str) => {
+        //         if (str.includes("原画")) return "原画";
+        //         if (str.includes("1080p")) return "1080p";
+        //         if (str.includes("720p")) return "720p";
+        //         if (str.includes("480p")) return "480p";
+        //         return str; // 其他情况
+        //     };
+        //
+        //     const keyA = getKey(a.level);
+        //     const keyB = getKey(b.level);
+        //
+        //     return (priorityOrder[keyA] || 10) - (priorityOrder[keyB] || 10);
+        // });
 
-    body.data.video_list = videoList
+        body.data.video_list = videoList
+    }
+    body = JSON.stringify(body);
+    $done({body});
+} catch (e) {
+    body = JSON.stringify(body);
+    $done({body});
 }
-body = JSON.stringify(body);
-$done({body});
-
 
 
 
