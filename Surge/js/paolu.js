69b8e5718bb87e00d578eab9b6c725bfb5d6f4d7
baixiaofei <guyuqiang@qq.com>
2025-11-06 14:01:55 +0800

    update

--- Surge/js/paolu.js
+++ Surge/js/paolu.js
@@ -1,14 +1,6 @@
 const url = $request.url;
 let body = JSON.parse($response.body);
 
-// 重命名
-const renameMap = {
-    "国产剧": "华语剧集"
-};
-body.MediaContainer.Directory.forEach(item => {
-    item.title = renameMap[item.title] || item.title
-})
-
 // 按此顺序排序
 const customOrder = [
     "动漫",
