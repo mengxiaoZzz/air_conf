3f342f65dfd9ef5004862e25e54325fd9433c174
baixiaofei <guyuqiang@qq.com>
2025-12-03 16:58:16 +0800

    update

--- Surge/js/emby-rename.js
+++ Surge/js/emby-rename.js
@@ -28,30 +28,30 @@ if (url.includes("meowfly")) {
     });
 }
 
-if (url.includes("wmu8.65194798.xyz")) {
-    // 按此顺序排序
-    const customOrder = [
-        "国产动漫",
-        "动漫剧集",
-        "国产剧集",
-        "欧美剧集",
-        "日本剧集",
-        "韩国剧集",
-        "亚太及小语种",
-        "华语电影",
-        "外语电影",
-        "动画电影",
-        "记录纪实",
-        "综艺节目",
-        "儿童剧集",
-        "特殊摄影",
-        "演唱会",
-        "合集"
-    ];
-    body.Items = body.Items.sort((a, b) => {
-        return customOrder.indexOf(a.Name) - customOrder.indexOf(b.Name);
-    });
-}
+// if (url.includes("wmu8.1.xyz")) {
+//     // 按此顺序排序
+//     const customOrder = [
+//         "国产动漫",
+//         "动漫剧集",
+//         "国产剧集",
+//         "欧美剧集",
+//         "日本剧集",
+//         "韩国剧集",
+//         "亚太及小语种",
+//         "华语电影",
+//         "外语电影",
+//         "动画电影",
+//         "记录纪实",
+//         "综艺节目",
+//         "儿童剧集",
+//         "特殊摄影",
+//         "演唱会",
+//         "合集"
+//     ];
+//     body.Items = body.Items.sort((a, b) => {
+//         return customOrder.indexOf(a.Name) - customOrder.indexOf(b.Name);
+//     });
+// }
 
 body = JSON.stringify(body);
 $done({body});
