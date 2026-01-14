5dbacdbb98a441fc25463f3a2952b0fd3693f2b8
baixiaofei <guyuqiang@qq.com>
2026-01-14 12:03:24 +0800

    update

--- Surge/js/paolu.js
+++ Surge/js/paolu.js
@@ -3,15 +3,14 @@ let body = JSON.parse($response.body);
 
 // 按此顺序排序
 const customOrder = [
-    "动漫",
-    "华语剧集",
+    "动画",
+    "国语剧集",
     "外语剧集",
-    "华语电影",
+    "国语电影",
     "外语电影",
     "动画电影",
-    "综艺节目",
-    "瑟瑟专区",
-    "里番"
+    "综艺",
+    "日本AV"
 ];
 body.MediaContainer.Directory = body.MediaContainer.Directory.sort((a, b) => {
     return customOrder.indexOf(a.title) - customOrder.indexOf(b.title);
