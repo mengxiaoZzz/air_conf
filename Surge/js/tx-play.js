5876cbed7ddf8300d67cbdafc5230e9819dcb1f7
baixiaofei <guyuqiang@qq.com>
2026-04-01 17:46:25 +0800

    update

--- Surge/js/tx-play.js
+++ Surge/js/tx-play.js
@@ -8,8 +8,9 @@ const senPlayerLink = `SenPlayer://x-callback-url/play?url=${encodeURIComponent(
 // 2. 构建 Surge 的 open-url 协议
 const surgeLink = `surge:///open-url?url=${encodeURIComponent(senPlayerLink)}`;
 // 3. 发送通知，并在通知中携带跳转指令
-$.notify("正在准备播放", "即将跳转到 SenPlayer", "", {
+$notification.post("正在准备播放", "即将跳转到 SenPlayer", "", {
     "open-url": surgeLink
 });
+
 
 $done({body});
