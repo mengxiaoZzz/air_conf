82ab5ccf232ab07127a8a6cb1dfb4f171004d125
baixiaofei <guyuqiang@qq.com>
2026-04-01 17:37:21 +0800

    update

---
+++ Surge/js/tx-play.js
@@ -0,0 +1,15 @@
+const url = $request.url;
+let body = JSON.parse($response.body);
+
+// 播放地址
+const videoUrl = $request.url;
+// 1. 构建 SenPlayer 的 URL Scheme
+const senPlayerLink = `SenPlayer://x-callback-url/play?url=${encodeURIComponent(videoUrl)}`;
+// 2. 构建 Surge 的 open-url 协议
+const surgeLink = `surge:///open-url?url=${encodeURIComponent(senPlayerLink)}`;
+// 3. 发送通知，并在通知中携带跳转指令
+$.notify("正在准备播放", "即将跳转到 SenPlayer", "", {
+    "open-url": surgeLink
+});
+
+$done({body});
