f55e681723ca29ac10a71ea125ead526b7b3d3f5
baixiaofei <guyuqiang@qq.com>
2026-06-17 19:08:03 +0800

    update

---
+++ Surge/js/tx-play_loon.js
@@ -0,0 +1,38 @@
+const url = $request.url;
+
+console.log("$argument");
+console.log($argument);
+
+// 播放器相关参数
+const PLAYER_MAP = {
+    "SenPlayer": {scheme: "SenPlayer://x-callback-url/play?url="},
+    "SenPlayer-dl": {scheme: "SenPlayer://x-callback-url/download?url="}
+};
+
+let player = $argument.arg1;
+console.log("player");
+console.log(player);
+if (player === undefined || player === "" || player === null) {
+    player = "SenPlayer"
+}
+let scheme = PLAYER_MAP[player].scheme
+console.log("scheme");
+console.log(scheme);
+
+// 播放地址
+const videoUrl = $request.url;
+
+// user-agent
+let userAgent = $request.headers["user-agent"]
+
+if (userAgent.includes("SenPlayer")) {
+    $done({});
+}
+// 1. 构建 SenPlayer 的 URL Scheme
+const senPlayerLink = scheme + `${encodeURIComponent(videoUrl)}`;
+// 2. 发送通知，并在通知中携带跳转指令
+$notification.post("ʚ前方高能预警ɞ", "翩若惊鸿，婉若游龙。", "", {
+    "url": senPlayerLink
+});
+
+$done({});
