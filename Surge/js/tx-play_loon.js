824c742768e5bda92acca03e98df0e855e40d508
baixiaofei <guyuqiang@qq.com>
2026-06-17 19:58:27 +0800

    update

--- Surge/js/tx-play_loon.js
+++ Surge/js/tx-play_loon.js
@@ -9,23 +9,19 @@ const PLAYER_MAP = {
     "SenPlayer-dl": {scheme: "SenPlayer://x-callback-url/download?url="}
 };
 
-let player = $argument.arg1;
+let player = $argument["player"]
 console.log("player===" + player);
-
-let player2 = $argument["player"]
-console.log("player2===" + player2);
 
 if (player === undefined || player === "" || player === null) {
     player = "SenPlayer"
 }
 let scheme = PLAYER_MAP[player].scheme
-console.log("scheme===" + scheme);
 
 // 播放地址
 const videoUrl = $request.url;
 
 // user-agent
-let userAgent = $request.headers["user-agent"]
+let userAgent = $request.headers["User-Agent"]
 console.log("$request.headers===" + $request.headers);
 console.log($request.headers);
 console.log("userAgent===" + userAgent);
@@ -36,8 +32,13 @@ if (userAgent.includes("SenPlayer")) {
 // 1. 构建 SenPlayer 的 URL Scheme
 const senPlayerLink = scheme + `${encodeURIComponent(videoUrl)}`;
 // 2. 发送通知，并在通知中携带跳转指令
-$notification.post("ʚ前方高能预警ɞ", "翩若惊鸿，婉若游龙。", "", {
-    "url": senPlayerLink
+
+let title = "ʚ前方高能预警ɞ"
+let subtitle = "翩若惊鸿，婉若游龙。"
+let content = ""
+$notification.post(title, subtitle, content, {
+    "url": senPlayerLink,
+    "openUrl": senPlayerLink,
 });
 
 $done({});
